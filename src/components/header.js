350dbafc2c9bbeb8d3c0936f95caa99eb18f478e
Manuel Scholz <manuel.serrano943@gmail.com>
2023-04-13 21:33:00 +0200

    Feat: Logo en cabecera y arreglo estilos

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import styles from '../styles/header.module.scss'
+import Logo from './logo'
 
 
 export default function Header() {
 
     return (
         <header className={styles.root}>
-
+            <Logo />
         </header>
     )
 }
