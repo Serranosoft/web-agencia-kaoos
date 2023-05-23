ae426751a3f473e47618af70fb504f2ee6c5e8c0
Manuel Scholz <manuel.serrano943@gmail.com>
2023-05-23 10:00:11 +0200

    Feat: Agregar enlaces a cabecera, eliminar código innecesario

--- src/components/content/button.js
+++ src/components/content/button.js
@@ -1,33 +1,24 @@
 import styles from '@/styles/components/content/button.module.scss'
+import Link from 'next/link'
 
-export default function Button({ children, displaySvg, className, highlight }) {
+export default function Button({ children, className, highlight, to }) {
 
 
     return (
         <>
+            {
+                to ? 
+                    <Link href={to} className={`${styles.button} ${highlight && styles.highlight} ${className && className}`}>
+                        {children}
+                    </Link>
+                :
+                <button className={`${styles.button} ${className && className} ${highlight && styles.highlight}`}>
+                    {children}
+                </button>
 
-            <button className={`${styles.button} ${className && className} ${highlight && styles.highlight}`}>
-                {displaySvg ?
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        className="sparkle"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            fill="none"
-                            stroke="black"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M14.187 8.096 15 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L21.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09L15 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L8.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09l.001-.001ZM6 14.25l-.259 1.035a3.375 3.375 0 0 1-2.456 2.456L2.25 18l1.035.259a3.375 3.375 0 0 1 2.456 2.456L6 21.75l.259-1.035a3.375 3.375 0 0 1 2.455-2.456L9.75 18l-1.036-.259a3.375 3.375 0 0 1-2.455-2.456L6 14.25ZM6.5 4l-.197.591a1.125 1.125 0 0 1-.712.712L5 5.5l.591.197a1.125 1.125 0 0 1 .712.712L6.5 7l.197-.591a1.125 1.125 0 0 1 .712-.712L8 5.5l-.591-.197a1.125 1.125 0 0 1-.712-.712L6.5 4Z"
-                        />
-                    </svg>
-                    :
-                    <></>
-
-                }
-                <span>{children}</span>
-            </button>
+                
+            }
         </>
+        
     )
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,8 @@ import styles from '@/styles/header/header.module.scss'
 import Logo from '@/components/logo/logo'
 import Text from '../content/text'
 import { useRouter } from 'next/router'
+import Button from '../content/button';
+import Link from 'next/link';
 
 export default function Header() {
 
@@ -9,10 +11,18 @@ export default function Header() {
 
     return (
         <header className={`${styles.root} ${route.pathname.split('/')[1] === "blog" && styles.light}`}>
-            <Logo />
+            <div>
+                <Link href="/">
+                    <Logo />
+                </Link>
+                {
+                    route.pathname.split('/')[1] === "blog" && <Link href="/blog"><Text>Blog</Text></Link>
+                }
+            </div>
             {
-                route.pathname.split('/')[1] === "blog" && <Text>Blog</Text>
+                route.pathname.split('/')[1] !== "blog" && <Button highlight to="/blog">Ver Blog</Button>
             }
+
         </header>
     )
 }
