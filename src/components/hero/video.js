a227c8fc381b29fba6baf99cb9fd31bfeb9b991e
Manuel Scholz <manuel.serrano943@gmail.com>
2023-06-04 17:52:34 +0200

    Feat: Componente video para decorar sección (Todavía sin aprovechar)

---
+++ src/components/hero/video.js
@@ -0,0 +1,21 @@
+import styles from "@/styles/hero/video.module.scss"
+import { useEffect, useRef } from "react"
+
+export default function Video() {
+
+    const ref = useRef();
+
+    useEffect(() => {
+        setTimeout(() => {
+            ref.current.play();
+        }, 2000);
+    }, [])
+
+    return (
+        <div className={styles.root}>
+            <video ref={ref} width="100%" height="auto" loading="eager" muted preload="auto" playsInline="">
+                <source src="video.webm" type="video/webm" />
+            </video>
+        </div>
+    )
+}
